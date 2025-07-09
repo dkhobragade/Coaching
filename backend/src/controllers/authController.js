74c90c5814a1b81975046ce75780450ae1c0a37d
Dikshant <dikshantkhobragade12@gmail.com>
2025-07-09 14:52:26 +0530

    added the backend support for the signin and logout

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,7 @@
 import User from "../models/User.js";
 import bcryptjs from "bcryptjs";
+import { generateToken } from "../utils/utils.js";
+import bcrypt from "bcryptjs";
 
 export const signUp = async (req, res) => {
   const { fullName, email, mobile, password } = req.body;
@@ -27,7 +29,7 @@ export const signUp = async (req, res) => {
     });
 
     if (newUser) {
-      generateToken(newUser._id, res);
+      generateToken({ userId: newUser._id, res });
       await newUser.save();
 
       res.status(201).json({
@@ -42,14 +44,45 @@ export const signUp = async (req, res) => {
     }
   } catch (error) {
     console.log("Error in SignUp controller", error.message);
-    res.status(50).json({ message: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
-export const signin = (req, res) => {
-  res.send("signin");
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Wrong Password" });
+    }
+
+    generateToken({ userId: user._id, res });
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+    });
+  } catch (error) {
+    console.log("Error in Signin controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("logout");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logout Successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
