555ce5e36dbd4d201e8451136471477d2ac85d21
Dikshant <dikshantkhobragade12@gmail.com>
2025-07-09 15:06:35 +0530

    added the /check for auth [ if the page gets refresh check if the user is authenticated or not ]

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { logout, signin, signUp } from "../controllers/authController.js";
+import {
+  check,
+  logout,
+  signin,
+  signUp,
+} from "../controllers/authController.js";
+import { protectRoute } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signin", signin);
 router.post("/signup", signUp);
 router.post("/logout", logout);
+
+router.get("/check", protectRoute, check);
 
 export default router;
