38fc662c8a0e7d44a161ad60901d1433a2a02e4b
Dikshant <dikshantkhobragade12@gmail.com>
2025-07-20 15:12:03 +0530

    added the deleted icon in the product table in order to  delete the particular product for admin only

--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -1,15 +1,17 @@
 "use client"
 
 import { useEffect, useState } from "react";
-import { Box, Button, Stack, styled, Typography } from "@mui/material";
+import { Box, Button, IconButton, Stack, styled, Typography } from "@mui/material";
 import { fetchWrapper } from "@/lib/fetchWrapper";
 import { toast } from "react-toastify";
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridActionsCellItem, GridColDef, GridRowParams } from '@mui/x-data-grid';
 import InputField from "@/components/lowLevelComponent/InputField";
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import { postWrapper } from "@/lib/postWrapper";
 import { ScaleButton } from "@/components/lowLevelComponent/Animation";
 import Image from "next/image";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { error } from "console";
 
 const columns = [
     { field: 'fullName', headerName: 'First name', width: 130 },
@@ -60,11 +62,74 @@ export default function Admin ()
     } );
     const [ isLoading, setIsLoading ] = useState<boolean>( false )
     const [ selectedImg, setSelectedImg ] = useState<string | any>( null )
+    const [ productList, setProductList ] = useState<any[]>( [] )
 
     useEffect( () =>
     {
         getUserList()
+        getBookList()
     }, [] )
+
+    const Bookscolumns: GridColDef[] = [
+        { field: 'name', headerName: 'Name', width: 130 },
+        {
+            field: 'description',
+            headerName: 'Description',
+            width: 250,
+        },
+        {
+            field: 'category',
+            headerName: 'Category',
+            width: 150,
+        },
+        {
+            field: 'price',
+            headerName: 'Price',
+            width: 160,
+        },
+        {
+            field: 'action',
+            headerName: 'Action',
+            width: 100,
+            type: 'actions',
+            getActions: ( params: GridRowParams ) => [
+                <GridActionsCellItem
+                    icon={ <DeleteIcon /> }
+                    label="Delete"
+                    onClick={ () => handleDelete( params.row._id ) }
+                />,
+            ],
+
+        }
+    ];
+
+
+    const handleDelete = ( id: string ) =>
+    {
+        postWrapper( 'auth/del-product', { id: id } ).then( ( resp ) =>
+        {
+            toast.success( resp.message )
+        } ).catch( ( error ) =>
+        {
+            toast.error( error.message )
+        } ).finally( () =>
+        {
+            getBookList()
+        } )
+    }
+
+
+    const getBookList = () =>
+    {
+        fetchWrapper( 'auth/products' ).then( ( resp ) =>
+        {
+            console.log( "products", resp )
+            setProductList( resp.items )
+        } ).catch( ( error ) =>
+        {
+            toast.error( error.message )
+        } )
+    }
 
     const getUserList = () =>
     {
@@ -155,6 +220,12 @@ export default function Admin ()
                     All Users
                 </Typography>
                 <DataGrid columns={ columns } rows={ userList } getRowId={ ( row ) => row._id } />
+            </Box>
+            <Box>
+                <Typography fontSize={ 20 } fontWeight={ 500 } >
+                    All Books
+                </Typography>
+                <DataGrid columns={ Bookscolumns } rows={ productList } getRowId={ ( row ) => row._id } />
             </Box>
             <Box>
                 <Typography fontSize={ 20 } fontWeight={ 500 }>
