60bdff37238d2b7a93ffac60cd4b83204d0a9625
Dikshant <dikshantkhobragade12@gmail.com>
2025-07-24 13:24:52 +0530

    minor change for the token issue in the production

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,6 +15,7 @@ const app = express();
 
 app.use(
   cors({
+    // origin: "http://localhost:3000",
     origin: /^https:\/\/coaching-.*\.vercel\.app$/,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
