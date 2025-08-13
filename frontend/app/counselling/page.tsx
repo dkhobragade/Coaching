ee04a46ae3076c190e4d908aa2f9b09f659d10d5
Dikshant <dikshantkhobragade12@gmail.com>
2025-08-13 23:49:51 +0530

    added the partical counselling hero section

--- frontend/app/counselling/page.tsx
+++ frontend/app/counselling/page.tsx
@@ -2,10 +2,12 @@ import { Box } from "@mui/material";
 import dynamic from "next/dynamic";
 
 const CounsellingLandingPage = dynamic( () => import( '@/components/Pages/CounsellingLandingPage' ) )
+const CounsellingHeroSection = dynamic( () => import( '@/components/Pages/CounsellingHeroSection' ) )
 
 export default function Counselling ()
 {
     return <Box minWidth="100%" padding={ 2 } minHeight="fit-content">
         <CounsellingLandingPage />
+        <CounsellingHeroSection />
     </Box>
 }
