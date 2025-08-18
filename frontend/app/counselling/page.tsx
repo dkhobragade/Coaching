9f5606c6a927232b4c94420e2602fbf194f800ac
Dikshant <dikshantkhobragade12@gmail.com>
2025-08-18 21:49:05 +0530

    added the counseeling Booking Section

--- frontend/app/counselling/page.tsx
+++ frontend/app/counselling/page.tsx
@@ -3,11 +3,13 @@ import dynamic from "next/dynamic";
 
 const CounsellingLandingPage = dynamic( () => import( '@/components/Pages/CounsellingLandingPage' ) )
 const CounsellingHeroSection = dynamic( () => import( '@/components/Pages/CounsellingHeroSection' ) )
+const CounsellingBookingSession = dynamic( () => import( '@/components/Pages/CounsellingBookingSection' ) )
 
 export default function Counselling ()
 {
     return <Box minWidth="100%" padding={ 2 } minHeight="fit-content">
         <CounsellingLandingPage />
         <CounsellingHeroSection />
+        <CounsellingBookingSession />
     </Box>
 }
